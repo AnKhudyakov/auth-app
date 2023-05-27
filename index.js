2ab8bb8f194957d0547423c088be1868c2b8dcbc
AnKhudyakov <andrew_job@list.ru>
2023-05-27 23:02:45 +0400

    add env client url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:8080"],
+    origin: ["http://localhost:3000", "http://localhost:8080", process.env.CLIENT_URL],
   })
 );
 app.use(express.json());
